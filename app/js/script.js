0414239733040c8d4d266c69fdc675a79d6eb928
Russell <russelldev@protonmail.com>
2020-12-06 09:36:49 +0000

    Add _header.scss | _animations.scss - fade out

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay')
 
 btnHamburger.addEventListener('click', ()=>{
     console.log('open');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ // close hamburger
         header.classList.remove('open');
-    } else {
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+
+    } else { // opem hamb menu
         header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
 
